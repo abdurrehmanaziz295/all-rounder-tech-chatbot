6d809791b23ed4f876250122e3e6e5923759e354
abdurrehmanaziz295 <abdurrehmanaziz444@gmail.com>
2025-10-18 02:03:36 +0500

    Update pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component data-filename="pages/ChatbotDetail" data-linenumber="537" data-visual-selector-id="pages/ChatbotDetail537" {...pageProps} />;
+  return <Component data-filename="pages/ChatbotDetail" data-linenumber="580" data-visual-selector-id="pages/ChatbotDetail580" {...pageProps} />;
 }
 
 export default MyApp;
