2da8f2f29f5a235078e8a83b9ee19049971ed063
abdurrehmanaziz295 <abdurrehmanaziz444@gmail.com>
2025-10-18 01:56:41 +0500

    Update pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import ChatInterface from '../components/ChatInterface';
+
+export default function Home() {
+  return (
+    <div data-filename="pages/ChatbotDetail" data-linenumber="525" data-visual-selector-id="pages/ChatbotDetail525" style={{
+      minHeight: '100vh',
+      background: 'linear-gradient(to bottom right, #f8fafc, #f1f5f9, #e0e7ff)'
+    }}>
+      <ChatInterface data-filename="pages/ChatbotDetail" data-linenumber="529" data-visual-selector-id="pages/ChatbotDetail529" />
+    </div>
+  );
+}
